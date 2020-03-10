7023a87cabfc1b5f48294c10ccb2b71ec66b9fb8
Deborah Fiammetti <fiammetti1@vafl-o-d02-14.unilim.fr>
2020-03-10 13:38:22 +0100

    landing page layout

---
+++ equipe.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DUNS - Equipe</title>
+    <meta name="description" content="Description site DUENES">
+    <meta name="keywords" content="mots clef associe a duenes">
+    <link rel="stylesheet" type="text/css" href="public/reset.css">
+    <link rel="stylesheet" type="text/css" href="public/main.css">
+    <script src="public/bundle.js"></script>
+</head>
+
+<body>
+
+<?php include('nav.php'); ?>
+<div class="left-nav">
+<h2><span class="hightlight">DUNS<span></h2>
+<nav>
+<ul>
+<li class="nav-item active">
+<a class="nav-link blue uppercase" href="#">Qui sommes nous<span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item">
+<a class="nav-link blue uppercase" href="#">Concept</span></a>
+</li>
+<li class="nav-item">    
+<a class="nav-link blue uppercase" href="#">Equipe</a> 
+</li>
+</ul>
+</nav>
+</div>
+<div class="container">
+<div class="row">
+    <div class="col-4">
+<p> Laurent abcd </p>
+<p></p>
+    </div>
+    <div class="col-4">
+        <p>Claire undeuxtrois</p>
+        </div>
+</div>
+</div>
---
+++ nav.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light main-nav" style="background: rgba(0,0,0,0)!important;">
+    <a class="navbar-brand" href="#"><img alt="logo duenes" src="assets/img/logo.png"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">DUNS<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Formation</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ressource</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Expertise</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Action</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Article</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Agenda</a>
+            </li>
+        </ul>
+    </div>
+</nav>
