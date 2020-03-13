d569f18e1ba6e411d818652301f2a760ec7e6225
Deborah Fiammetti <fiammetti1@vafl-o-d02-14.unilim.fr>
2020-03-13 14:11:34 +0100

    style landing page update

--- index.php
+++ index.php
@@ -98,15 +98,15 @@
         <div class="row">
             <div class="col-5">
                 <div class="row h-100 row-cols-1">
-                    <button class="col slider-title px-5">
+                    <button class="col slider-title px-20perc">
                         <h4>Un facilitateur de projets pédagogiques</h4>
                         <p>Des outils spécifiques au secteur de la santé. Nous proposons des ateliers numériques pour développer vos capacités et connaissances digitales.</p>
                     </button>
-                    <button class="col slider-title px-5">
+                    <button class="col slider-title px-20perc">
                         <h4>Des solutions numériques adaptées</h4>
                         <p>Nous mettons à votre disposition un large choix de matériels pour répondre au mieux à vos attentes.  Nous proposons des ateliers numériques et des connaissances digitales.</p>
                     </button>
-                    <button class="col slider-title px-5">
+                    <button class="col slider-title px-20perc">
                         <h4>Des outils spécifiques au secteur de la santé</h4>
                         <p>Nous apportons une approche moderne et numérique aux métiers de la santé. Je remercie toute l’équipe de DUENES pour leur accompagnement dans mon projet. </p>
                     </button>
@@ -137,28 +137,34 @@
 
     <section class="container-fluid p-5">
             <div class="row no-gutters">
-                <h2>SUIVEZ TOUTE <br> L’actualité de duenes</h2>
+                <h2><span class="hightlight">SUIVEZ</span> TOUTE <br> L’actualité de duenes</h2>
             </div>
 
             <div class="row no-gutters">
                 <p class="subtitle mb-5">Nos derniers articles</p>
-            <div>
+            </div>
 
             <div class="row no-gutters">
                 <article class="col-3 mx-auto">
                     <img class="w-100" alt="image article" src="assets/img/article1.jpg">
-                    <time>13 JAN 2020</time>
-                    <h4>Outil Screenshot pour effectuer des captures d’écrans </h4>
+                        <div class="position-relative">
+                            <time class="background-white "><span>13 JAN 2020</span></time>
+                        </div>
+                    <h4 class="mt-5">Outil Screenshot pour effectuer des captures d’écrans </h4>
                 </article>
                 <article class="col-3 mx-auto">
                     <img class="w-100" alt="image article" src="assets/img/article2.jpg">
-                    <time>08 JAN 2020</time>
-                    <h4>Modèle de ponction lombaire</h4>
+                    <div class="position-relative">
+                            <time class="background-white "><span>02 FEV 2020</span></time>
+                        </div>
+                    <h4 class="mt-5">Modèle de ponction lombaire</h4>
                 </article>
                 <article class="col-3 mx-auto">
                     <img class="w-100" alt="image article" src="assets/img/article3.jpg">
-                    <time>21 NOV 2019</time>
-                    <h4>Séminaire de pédagogie médicale CCA-AHU </h4>
+                        <div class="position-relative">
+                            <time class="background-white "><span>21 oct 2020</span></time>
+                        </div>
+                    <h4 class="mt-5">Séminaire de pédagogie médicale CCA-AHU </h4>
                 </article>
             </div>
             <div class="row justify-content-center">
@@ -168,19 +174,19 @@
             </div>
     </section>
 
-    <section class="container-fluid no-gutters background-white vh-50">
+    <section class="container-fluid no-gutters background-white vh-50 px-0">
         <div class="cointainer h-100">
             <div class="row no-gutters justify-content-around h-100 align-items-center py-5">
-                <div class="offset-1 col-3">
-                    <h2>Des pôles de compétences <br> variés à Votre service </h2>
+                <div class="offset-1 col-3 h-75 d-flex flex-column justify-content-around">
+                    <h2><span class="hightlight">Des pôles</span> de compétences <br> variés à Votre service </h2>
                     <p>Un projet... Une solution.</p>
                     <a class="a-button">Découvrir les pôles</a>
                 </div>
                 <div class="offset-1 col-5 mr-auto">
-                    <blockquote>
+                    <blockquote class="row">
                         <p>Je remercie toute l’équipe de DUENES pour leur accompagnement dans mon projet. Grâce à leur aide, j’ai pu mettre en place un espace de simulation pédagogique à l’intention de mes étudiants. 
                         </p>
-                        <footer>—Philippe DURAND, <cite>Enseignant chercheur en Odontologie</cite></footer>
+                        <footer> <em class="text-uppercase">Philippe DURAND </em><br><cite>Enseignant chercheur en Odontologie</cite></footer>
                     </blockquote>
                 </div>
             </div>
@@ -189,11 +195,16 @@
 
     <section class="container-fluid no-gutters py-5">
         <div class="row no-gutters">
-            <div class="col-4">
-                <img class="w-100" alt="phootographie d'une formation numérique" src="assets/img/article3.jpg">
-                <h2>Les formations <br>proposées par duenes</h2>
-            </div>
             <div class="col-3">
+                <div class="row h-75">
+                    <img class="img-responsive" alt="phootographie d'une formation numérique" src="assets/img/article3.jpg">
+                </div>
+                <div class="row h-25 background-white">
+                    <h2>Les formations <br>proposées par duenes</h2>
+                </div>
+            </div>
+
+            <div class="col-4">
                 <h4>Les ateliers</h4>
                 <p>Nous proposons des ateliers numériques pour développer vos capacités et connaissances digitales. Ces enseignements dédiés aux professionnels de santé sont un véritable atout et favorise et incite la réflexion entre enseignants sur les évolutions pédagogiques</p>
                 <a class="d-block a-text">En savoir +</a>
